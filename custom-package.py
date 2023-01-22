5509026f041c66facd0de19cd2fca90048828cd3
MrBaselier <jjbas@xs4all.nl>
2023-01-22 12:30:36 +0100

    Update custom-package.py
    Added sniffer & multi-threading

--- custom-package.py
+++ custom-package.py
@@ -11,29 +11,44 @@
 from scapy.all import *
 import struct
 import binascii
+import threading
+import time
 
 
 ######## SETTINGS / VARIABLES ########
 #Setting variables
-#This setting is to specify the layer where tha package is send from. Options are "layer1 & layer2"
+#This setting is to specify the layer where the package is send from. Options are "layer1 & layer3"
 layer = "layer1"
 
 #Set layer to 1 if you want to manually control it. If 0 then the computer calculates this layer
-manual_layer_2 = 0
-manual_layer_3 = 0
+manual_layer_3 = 0 #IP / ICMP / ARP
+manual_layer_4 = 0 #TCP / UDP
 manual_layer_raw = 1
 
 #Source / Destination variables
-source_ip = 'x.x.x.x'
-destination_ip = 'x.x.x.x'
+source_ip = '192.168.178.30'
+destination_ip = '192.168.178.143'
 source_port = 'iso_tsap'
 destination_port = 36545
-source_mac = 'xx:xx:xx:xx:xx:xx'
-destination_mac = 'xx:xx:xx:xx:xx:xx'
+source_mac = '8c:f3:19:03:e4:b3' #PLC
+destination_mac = '8c:f3:19:00:a6:de' #HMI
 send_interface = "Ethernet 2"
 
 #Select if you want to loop the packet. 1 = yes and 0 = no
+#Normal loop - send packets synchronous
 loop = 0
+#Threaded loop - send packets asynchronous
+threaded = 0
+nr_of_threads = 20
+
+#Sniffing after sending. 1 = yes and 0 = no
+#Note - Since the return traffic is usually faster then the startup of the sniffing process you might need to resent the package (run this script) in another terminal while the sniffer is listening.
+sniffing = 0
+#Number of return packets to sniff before the sniffer stops
+nr_of_packets_to_sniff = 1
+#Store sniffed packets if sniffing is on. 1 = yes and 0 = no
+store_sniffed_packets = 1
+store_location = "C:\\Users\\Redux Gamer\\Desktop\\sniffed_packages.pcap"
 
 
 ######## FUNCTIONS ########
@@ -64,8 +79,126 @@ def int_to_bytes_4(value):
 
 #=========================================================
 #PROFINET
-#https://rt-labs.com/docs/p-net/profinet_details.html
+#https://github.com/rtlabs-com/p-net/blob/master/doc/profinet_basics.rst
 
+"""
+#EXAMPLE - Blink HMI screen
+profinet_frameid = int_to_bytes_2(65277) #fefd Hex
+profinet_serviceId = int_to_bytes_1(4)
+profinet_serviceType = int_to_bytes_1(0)
+#profinet_xid is usually not important to controll the hardware
+profinet_xid_1 = int_to_bytes_1(0)
+profinet_xid_2 = int_to_bytes_1(0)
+profinet_xid_3 = int_to_bytes_1(25) #19 Hex
+profinet_xid_4 = int_to_bytes_1(18) #12 Hex
+#profinet_reserved is usually not important to controll the hardware
+profinet_reserved = int_to_bytes_2(0)
+profinet_dcpdatalength = int_to_bytes_2(8)
+profinet_block_option_control = int_to_bytes_1(5)
+profinet_block_suboption_signal = int_to_bytes_1(3)
+profinet_block_dcpblocklength = int_to_bytes_2(4)
+profinet_block_blockqualifier = int_to_bytes_2(0)
+profinet_block_signalvalue_1 =int_to_bytes_1(1)
+profinet_block_signalvalue_2 =int_to_bytes_1(0)
+
+#Compile Raw part of Profinet package
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength + profinet_block_blockqualifier + profinet_block_signalvalue_1 + profinet_block_signalvalue_2
+
+
+#EXAMPLE - Reset to factory defaults
+profinet_frameid = int_to_bytes_2(65277)
+profinet_serviceId = int_to_bytes_1(4)
+profinet_serviceType = int_to_bytes_1(0)
+profinet_xid_1 = int_to_bytes_1(0)
+profinet_xid_2 = int_to_bytes_1(0)
+profinet_xid_3 = int_to_bytes_1(0)
+profinet_xid_4 = int_to_bytes_1(3)
+profinet_reserved = int_to_bytes_2(0)
+profinet_dcpdatalength = int_to_bytes_2(6)
+profinet_block_option_control = int_to_bytes_1(5)
+profinet_block_suboption_signal = int_to_bytes_1(6)
+profinet_block_dcpblocklength = int_to_bytes_2(2)
+profinet_block_blockqualifier = int_to_bytes_2(22)
+
+#Compile Raw part of Profinet package
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength + profinet_block_blockqualifier
+
+
+#EXAMPLE - Rename the device
+profinet_frameid = int_to_bytes_2(65277) 
+profinet_serviceId = int_to_bytes_1(4)
+profinet_serviceType = int_to_bytes_1(0)
+profinet_xid_1 = int_to_bytes_1(0)
+profinet_xid_2 = int_to_bytes_1(0)
+profinet_xid_3 = int_to_bytes_1(0)
+profinet_xid_4 = int_to_bytes_1(3)
+profinet_reserved = int_to_bytes_2(0)
+profinet_dcpdatalength = int_to_bytes_2(10)
+profinet_block_option_control = int_to_bytes_1(2)
+profinet_block_suboption_signal = int_to_bytes_1(2)
+profinet_block_dcpblocklength = int_to_bytes_2(6)
+profinet_block_blockqualifier = int_to_bytes_2(1)
+profinet_namestation = string_to_bytes("jarno")
+
+#Compile Raw part of Profinet package
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength + profinet_block_blockqualifier + profinet_namestation
+
+
+#EXAMPLE - Give device new IP settings
+profinet_frameid = int_to_bytes_2(65277)
+profinet_serviceId = int_to_bytes_1(4)
+profinet_serviceType = int_to_bytes_1(0)
+profinet_xid_1 = int_to_bytes_1(0)
+profinet_xid_2 = int_to_bytes_1(0)
+profinet_xid_3 = int_to_bytes_1(0)
+profinet_xid_4 = int_to_bytes_1(1)
+profinet_reserved = int_to_bytes_2(0)
+profinet_dcpdatalength = int_to_bytes_2(18)
+profinet_block_option_control = int_to_bytes_1(1)
+profinet_block_suboption_signal = int_to_bytes_1(2)
+profinet_block_dcpblocklength = int_to_bytes_2(14)
+profinet_block_blockqualifier = int_to_bytes_2(1)
+#profinet_namestation = string_to_bytes("jarno")
+#Give IP
+profinet_ip1 = int_to_bytes_1(192)
+profinet_ip2 = int_to_bytes_1(168)
+profinet_ip3 = int_to_bytes_1(178)
+profinet_ip4 = int_to_bytes_1(143)
+profinet_subnet1 = int_to_bytes_1(255)
+profinet_subnet2 = int_to_bytes_1(255)
+profinet_subnet3 = int_to_bytes_1(255)
+profinet_subnet4 = int_to_bytes_1(0)
+profinet_gateway1 = int_to_bytes_1(192)
+profinet_gateway2 = int_to_bytes_1(168)
+profinet_gateway3 = int_to_bytes_1(178)
+profinet_gateway4 = int_to_bytes_1(1)
+
+#Compile Raw part of Profinet package
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength + profinet_block_blockqualifier + profinet_ip1 + profinet_ip2 + profinet_ip3 + profinet_ip4 + profinet_subnet1 + profinet_subnet2 + profinet_subnet3 + profinet_subnet4 + profinet_gateway1 + profinet_gateway2 + profinet_gateway3 + profinet_gateway4
+
+
+#EXAMPLE - DCP IDENTIFY DEVICES
+profinet_frameid = int_to_bytes_2(65278)
+profinet_serviceId = int_to_bytes_1(5)
+profinet_serviceType = int_to_bytes_1(0)
+profinet_xid_1 = int_to_bytes_1(0)
+profinet_xid_2 = int_to_bytes_1(0)
+profinet_xid_3 = int_to_bytes_1(0)
+profinet_xid_4 = int_to_bytes_1(3)
+profinet_reserved = int_to_bytes_2(1)
+profinet_dcpdatalength = int_to_bytes_2(4)
+profinet_block_option_control = int_to_bytes_1(255)
+profinet_block_suboption_signal = int_to_bytes_1(255)
+profinet_block_dcpblocklength = int_to_bytes_2(0)
+
+#Compile Raw part of Profinet package
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength
+
+"""
+
+
+
+#https://rt-labs.com/docs/p-net/profinet_details.html
 #FrameID values:
 #8000 = 32768 decimal = Profinet cyclic - Output CR
 #8001 = 32769 decimal = Profinet cyclic - Input CR
@@ -75,57 +208,72 @@ def int_to_bytes_4(value):
 #fefd = 65277 decimal = Profinet acyclic, DCP - GET_SET
 #fefe = 65278 decimal = Profinet acyclic, DCP - Identify request
 #fefe = 65278 decimal = Profinet acyclic, DCP - IIdentify response
-profinet_frameid = int_to_bytes_2(65277) #fefd Hex - Profinet acyclic, DCP - GET_SET
+profinet_frameid = int_to_bytes_2(65278) #The Frame ID identifies what kind of frame this is. For any “Identify request” (see the next two fields) it needs to be set to 0xfefe, and for any “Identify response” to 0xfeff. fefd = Profinet acyclic, DCP - GET_SET
 #ServiceID defines the action on the specific device
-profinet_serviceId = int_to_bytes_1(4)
+profinet_serviceId = int_to_bytes_1(5)
 #0 = request response (request) -  1 = Response Success (answer)
 profinet_serviceType = int_to_bytes_1(0)
-#profinet_xid is usually not important to controll the hardware
+#profinet_xid is usually not important to controll the hardware. The Xid is a 32-bit random number which identifies this request. All responses need to contain the same ID for the receiver to figure out which request the just received response belongs to as there is no such thing as a connection on Ethernet Layer 2.
 profinet_xid_1 = int_to_bytes_1(0)
 profinet_xid_2 = int_to_bytes_1(0)
-profinet_xid_3 = int_to_bytes_1(25) #19 Hex
-profinet_xid_4 = int_to_bytes_1(18) #12 Hex
+profinet_xid_3 = int_to_bytes_1(0)
+profinet_xid_4 = int_to_bytes_1(3)
 #profinet_reserved is usually not important to controll the hardware
-profinet_reserved = int_to_bytes_2(0)
+profinet_reserved = int_to_bytes_2(1)
 #dcpdatalength defines the size of the Data block. Profinet DCP runs via “Profinet acyclic realtime”.
-profinet_dcpdatalength = int_to_bytes_2(8)
+profinet_dcpdatalength = int_to_bytes_2(4)
 #DATA BLOCK / CONTROL SIGNAL BLOCK
 #https://rt-labs.com/docs/p-net/profinet_details.html
 #Option and Suboption define the action to take. Note, this is in combination with the serviceID
 """
 Service IDs     Option  Suboption       Description     Contains
-3       1       1       MAC address     
-3, 4, 5, 6      1       2       IP parameter    IP address, netmask, gateway
-3, 4, 5, 6      1       3       Full IP suite   IP address, netmask, gateway, DNS
-3, 5    2       1       Type of station Device vendor
-3, 4, 5, 6      2       2       Name of station Also permanent/temporary
-3, 5, 6 2       3       Device ID       VendorID, DeviceID
-3, 5    2       4       Device role     ?
-3, 5    2       5       Device options  Which options are available
-Filter only     2       6       Alias name      
-6       2       8       OEM device ID   
-4       5       1       Start transaction       
-4       5       2       End transaction 
-4       5       3       Signal (Flash LED)      Flash once
-4       5       4       Response        
-4       5       6       Reset to factory        Type of reset
-5       255     255     All     
-6       6       1       Device initiative       Issues Hello at power on
+3               1       1               MAC address     
+3, 4, 5, 6      1       2               IP parameter    IP address, netmask, gateway
+3, 4, 5, 6      1       3               Full IP suite   IP address, netmask, gateway, DNS
+3, 5            2       1               Type of station Device vendor
+3, 4, 5, 6      2       2               Name of station Also permanent/temporary
+3, 5, 6         2       3               Device ID       VendorID, DeviceID
+3, 5            2       4               Device role     ?
+3, 5            2       5               Device options  Which options are available
+Filter only     2       6               Alias name      
+6               2       8               OEM device ID   
+4               5       1               Start transaction       
+4               5       2               End transaction 
+4               5       3               Signal (Flash LED)      Flash once
+4               5       4               Response        
+4               5       6               Reset to factory        Type of reset
+5               255     255             All     
+6               6       1               Device initiative       Issues Hello at power on
 """
-profinet_block_option_control = int_to_bytes_1(5)
-profinet_block_suboption_signal = int_to_bytes_1(3)
+profinet_block_option_control = int_to_bytes_1(255)
+profinet_block_suboption_signal = int_to_bytes_1(255)
 #Defines the size of the upcoming block
-profinet_block_dcpblocklength = int_to_bytes_2(4)
+profinet_block_dcpblocklength = int_to_bytes_2(0)
+
+#Use option when needed. When not needed, keep commented-out
 #0 = Save value temporary - 1 = Save value permanently
-profinet_block_blockqualifier = int_to_bytes_2(0)
-#Further defines the signal - 0x0100 (1-0) = Flash once - 
-profinet_block_signalvalue_1 =int_to_bytes_1(1)
-#profinet_block_signalvalue_2 is usually not important to controll the hardware
-profinet_block_signalvalue_2 =int_to_bytes_1(0)
+#For device reset use: 0(hex) = mode0 - Reset to Factory (clear all default values) - 2(hex) = mode1 - Reset application data - 4(hex) = mode2 - Reset communication parameters - 6(hex) = mode3 - Reset engineering parameter - 8(hex) = mode4 - Reset all stored data - 16(hex) = mode8 - Reset device - 18(hex) = mode9 - Reset & Restore data
+#profinet_block_blockqualifier = int_to_bytes_2(1)
+
+#Name the station
+#profinet_namestation = string_to_bytes("jarno")
+
+#Give IP
+#profinet_ip1 = int_to_bytes_1(192)
+#profinet_ip2 = int_to_bytes_1(168)
+#profinet_ip3 = int_to_bytes_1(178)
+#profinet_ip4 = int_to_bytes_1(143)
+#profinet_subnet1 = int_to_bytes_1(255)
+#profinet_subnet2 = int_to_bytes_1(255)
+#profinet_subnet3 = int_to_bytes_1(255)
+#profinet_subnet4 = int_to_bytes_1(0)
+#profinet_gateway1 = int_to_bytes_1(192)
+#profinet_gateway2 = int_to_bytes_1(168)
+#profinet_gateway3 = int_to_bytes_1(178)
+#profinet_gateway4 = int_to_bytes_1(1)
 
 #Compile Raw part of Profinet package
-Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength + profinet_block_blockqualifier + profinet_block_signalvalue_1 + profinet_block_signalvalue_2
-
+Profinet = profinet_frameid + profinet_serviceId + profinet_serviceType + profinet_xid_1 + profinet_xid_2 + profinet_xid_3 + profinet_xid_4 + profinet_reserved + profinet_dcpdatalength + profinet_block_option_control + profinet_block_suboption_signal + profinet_block_dcpblocklength
 
 #=========================================================
 #TPKT
@@ -257,46 +405,81 @@ lRaw = raw = Raw(load = payload)
 
 
 ######## PACKET CONSTRUCTION ########
-if layer == "layer1" and manual_layer_2 == 1 and manual_layer_3 == 1 and manual_layer_raw == 1:
+if layer == "layer1" and manual_layer_3 == 1 and manual_layer_4 == 1 and manual_layer_raw == 1:
         packet = l1 / l2 / l3 / lRaw
-if layer == "layer1" and manual_layer_2 == 1 and manual_layer_3 == 0 and manual_layer_raw == 1:
+if layer == "layer1" and manual_layer_3 == 1 and manual_layer_4 == 0 and manual_layer_raw == 1:
         packet = l1 / l2 / lRaw
-if layer == "layer1" and manual_layer_2 == 1 and manual_layer_3 == 1 and manual_layer_raw == 0:
+if layer == "layer1" and manual_layer_3 == 1 and manual_layer_4 == 1 and manual_layer_raw == 0:
         packet = l1 / l2 / l3
-if layer == "layer1" and manual_layer_2 == 1 and manual_layer_3 == 0 and manual_layer_raw == 0:
+if layer == "layer1" and manual_layer_3 == 1 and manual_layer_4 == 0 and manual_layer_raw == 0:
         packet = l1 / l2
-if layer == "layer1" and manual_layer_2 == 0 and manual_layer_3 == 1 and manual_layer_raw == 1:
+if layer == "layer1" and manual_layer_3 == 0 and manual_layer_4 == 1 and manual_layer_raw == 1:
         packet = l1 / l3 / lRaw
-if layer == "layer1" and manual_layer_2 == 0 and manual_layer_3 == 0 and manual_layer_raw == 1:
+if layer == "layer1" and manual_layer_3 == 0 and manual_layer_4 == 0 and manual_layer_raw == 1:
         packet = l1 / lRaw
-if layer == "layer1" and manual_layer_2 == 0 and manual_layer_3 == 1 and manual_layer_raw == 0:
+if layer == "layer1" and manual_layer_3 == 0 and manual_layer_4 == 1 and manual_layer_raw == 0:
         packet = l1/ l3
-if layer == "layer1" and manual_layer_2 == 0 and manual_layer_3 == 0 and manual_layer_raw == 0:
+if layer == "layer1" and manual_layer_3 == 0 and manual_layer_4 == 0 and manual_layer_raw == 0:
         packet = l1
 
-if layer == "layer2" and manual_layer_3 == 1 and manual_layer_raw == 1:
+if layer == "layer3" and manual_layer_4 == 1 and manual_layer_raw == 1:
         packet = l2 / l3 / lRaw
-if layer == "layer2" and manual_layer_3 == 1 and manual_layer_raw == 0:
+if layer == "layer3" and manual_layer_4 == 1 and manual_layer_raw == 0:
         packet = l2 / l3
-if layer == "layer2" and manual_layer_3 == 0 and manual_layer_raw == 1:
+if layer == "layer3" and manual_layer_4 == 0 and manual_layer_raw == 1:
         packet = l2 / lRaw
-if layer == "layer2" and manual_layer_3 == 0 and manual_layer_raw == 0:
+if layer == "layer3" and manual_layer_4 == 0 and manual_layer_raw == 0:
         packet = l2
 
 
 ######## SHOW AND SEND PACKET(S) ########
 #Show packet
+print("=== THIS PACKET IS GENERATED AND SEND ===")
 packet.show2()
+
+def thread_sendp(name):
+        sendp(packet,iface=send_interface)
+
+def thread_send(name):
+        send((packet),iface=send_interface)
 
 #Send Packet
 #Layer1 (control layer 1)
 if layer == "layer1" and loop == 0:
         sendp(packet,iface=send_interface)
 if layer == "layer1" and loop == 1:
-        sendp(packet,iface=send_interface, loop=1)
+        if threaded == 1:
+                #Send threaded packet
+                for index in range(nr_of_threads):
+                        ts = threading.Thread(target=thread_sendp, args=(1,))
+                        ts.start()
+        else:
+                #Just loop the packet
+                sendp(packet,iface=send_interface, loop=1)
 
 #Layer3 (automatic layer 1)
-if layer == "layer2" and loop == 0:
+if layer == "layer3" and loop == 0:
         send((packet),iface=send_interface)
-if layer == "layer2" and loop == 1:
-        send((packet),iface=send_interface, loop=1)
+if layer == "layer3" and loop == 1:
+        if threaded == 1:
+                #Send threaded packet
+                for index in range(nr_of_threads):
+                        ts = threading.Thread(target=thread_send, args=(1,))
+                        ts.start()
+        else:
+                #Just loop the packet
+                send((packet),iface=send_interface, loop=1)
+                
+
+######## PACKET SNIFFING ########
+def customSniffAction(packet):
+        packet.show2()
+
+if sniffing == 1:
+        print("=== SNIFFED PACKAGES (MAX = " + str(nr_of_packets_to_sniff) + " PACKET) ===")
+        capture = sniff(filter="ether src host " + destination_mac, prn=customSniffAction, count=nr_of_packets_to_sniff)
+
+        if store_sniffed_packets == 1:
+                wrpcap(store_location, capture)
+
+
